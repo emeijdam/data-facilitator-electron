7b1c36b9d0d39cb6e6762141a3b55b8de9eaecbf
emeijdam@ <emeijdam@dasc.nl>
2023-04-13 14:11:57 +0200

    right click

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,15 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+  window.onload = function() {
+    let {ipcRenderer} = require('electron');
+
+      window.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
+        ipcRenderer.send('show-context-menu', { x: e.clientX, y: e.clientY })
+      })
+
+      ipcRenderer.on('context-menu-command', (e, command, my) => {
+        ipcRenderer.sendSync('mouse', { x: my.x, y: my.y })
+      })
+  };
