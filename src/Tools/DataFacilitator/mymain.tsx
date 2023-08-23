acb7aa53f50b44841b0e46d9c949d5c1e2c799ab
emeijdam <emeijdam@dasc.nl>
2023-08-23 12:06:59 +0200

    news tsufff

--- src/Tools/DataFacilitator/mymain.tsx
+++ src/Tools/DataFacilitator/mymain.tsx
@@ -2,127 +2,252 @@ import { Button, Dialog, DialogActions, DialogBody, DialogContent, DialogSurface
 import { useReducer, useState } from "react";
 import ConnectorPicker from "./ConnectorPicker";
 import Tabulate from "../_datahelpers/Tabulate";
+import { nanoid } from 'nanoid';
 
 const useStyles = makeStyles({
     dit: {
-     
+
         //height: '100%;',
-       // ...shorthands.overflow('scroll'),
-      
+        // ...shorthands.overflow('scroll'),
     },
 })
 
-const initialDataTables = [
-    {
-        name: 'CBS Table Observation',
-        description: 'beschrijving',
-        type: 'odata4',
-        connectorConfiguration: {
-            url: ''
-        }
-    },
-    {
-        name: 'CBS Table Metadata',
-        description: 'beschrijving',
-        type: 'odata4',
-        connectorConfiguration: {
-            url: ''
-        }
-    },
-]
-
-const stateding = 
-    {
-       "imports": initialDataTables,
-       "exports": initialDataTables 
-    }
-
-
-
-enum UserActionType {
-    ADD = "addImport"
-  }
-
-interface Connection {
-    name: string,
-    description: string,
-    type: string
-    connectorConfiguration : {url: string}
+/// between 
+enum nodeClass {
+    SOURCENODE,
+    EXPORTNODE
 }
 
-type ImportConnectionsSTATE = Connection[];
+enum FlowActionType {
+    ADDNODE,
+    DELETENODE,
+    EXECUTEFLOW
+}
 
-const initialImportConnections: ImportConnectionsSTATE = initialDataTables;
+type FlowAction =
+    | { type: FlowActionType.ADDNODE; node: FlowNode }
+    | { type: FlowActionType.DELETENODE; nodeID: string }
 
-type UserAction =
-| { type: UserActionType.ADD; connection: Connection }
+type FlowNode = {
+    nodeID: string,
+    nodeClass: nodeClass,
+    name: string,
+    description: string,
+    type: string,
+    //properties: []
+}
 
-function userReducer(state: ImportConnectionsSTATE, action: UserAction): ImportConnectionsSTATE {
-    switch (action.type) {
-      case UserActionType.ADD:
-        console.log(state)
-        return [...state, action.connection];
-      default:
-        throw new Error();
-    }
+
+type initFlowState ={
+    nodes: FlowNode[],
   }
 
+
+const initialFlowState = 
+    {
+    nodes: [
+        {
+            nodeID: nanoid(),
+            nodeClass: nodeClass.SOURCENODE,
+            name: 'CBS Table Observation',
+            description: 'beschrijving',
+            type: 'odata4',
+            // properties: {
+            //     url: ''
+            // }
+        },
+        {
+            nodeID: nanoid(),
+            nodeClass: nodeClass.EXPORTNODE,
+            name: 'SPSS Save',
+            description: 'beschrijving',
+            type: 'SPSS',
+            // properties: {
+            //     filename: 'c:\\temp\\spssfile.sav'
+            // }
+        },
+    ]
+}
+
+
+
+
+function flowReducer(state: initFlowState, action: FlowAction): initFlowState {
+  //  const { type, payload } = action;
+    switch (action.type) {
+        case FlowActionType.ADDNODE:
+            console.log(state);
+            return {nodes: [...state.nodes, action.node]}
+        case FlowActionType.DELETENODE:
+            console.log(state)
+          //  items: state.items.filter((item) => item.id !== action.payload),
+          
+           // return state.filter( (node) => node.nodeID !== action.nodeID );
+            return {nodes: [...state.nodes.filter((node) => node.nodeID !== action.nodeID )]}
+        default:
+            throw new Error();
+    }
+}
+///
+
+
+// const initialDataTables = [
+//     {
+//         nodeID: nanoid(),
+//         name: 'CBS Table Observation',
+//         description: 'beschrijving',
+//         type: 'odata4',
+//         connectorConfiguration: {
+//             url: ''
+//         }
+//     },
+//     {
+//         nodeID: nanoid(),
+//         name: 'CBS Table Metadata',
+//         description: 'beschrijving',
+//         type: 'odata4',
+//         connectorConfiguration: {
+//             url: ''
+//         }
+//     },
+// ]
+
+// const stateding =
+// {
+//     "imports": initialDataTables,
+//     "exports": initialDataTables
+// }
+
+
+
+// enum UserActionType {
+//     ADD = "addImport",
+//     DELETE = "Delete"
+// }
+
+// interface Connection {
+//     nodeID: string,
+//     name: string,
+//     description: string,
+//     type: string
+//     connectorConfiguration: { url: string }
+// }
+
+// type ImportConnectionsSTATE = Connection[];
+
+// const initialImportConnections: ImportConnectionsSTATE = initialDataTables;
+
+// type UserAction =
+//     | { type: UserActionType.ADD; connection: Connection }
+//     | { type: UserActionType.DELETE; nodeID: string }
+
+// function userReducer(state: ImportConnectionsSTATE, action: UserAction): ImportConnectionsSTATE {
+//     switch (action.type) {
+//         case UserActionType.ADD:
+//             console.log(state)
+//             return [...state, action.connection];
+//         case UserActionType.DELETE:
+//             console.log(state)
+//           //  items: state.items.filter((item) => item.id !== action.payload),
+          
+//             return state.filter( (node) => node.nodeID !== action.nodeID );
+//         default:
+//             throw new Error();
+//     }
+// }
+
 const DOEN: React.FC = () => {
-   // const [dataTables, setDataTables] = useState(stateding)
-    const [imports, userDispatch] = useReducer(userReducer, initialImportConnections);
+    // const [dataTables, setDataTables] = useState(stateding)
+    const [flowDocument, flowActionDispatch] = useReducer(flowReducer, initialFlowState);
+   // const [imports, userDispatch] = useReducer(userReducer, initialImportConnections);
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
     //const [error, setError] = useState(null);
-  
-  
+
+
     const handleClick = () => {
-      setLoading(true);
-      // ?$filter=WijkenEnBuurten eq \'GM0363\'
-      const tableid = '03763';
-  
-      fetch(`https://odata4.cbs.nl/CBS/${tableid}/Observations`)
-        .then(response => response.json())
-        .then((usefulData) => {
-          setLoading(false);
-          setData(usefulData.value);
-          //setData(usefulData.value.slice(0,9));
-  
-        })
-        .catch((e) => {
-          console.error(`An error occurred: ${e}`)
-        });
+        setLoading(true);
+        // ?$filter=WijkenEnBuurten eq \'GM0363\'
+        const tableid = '03763';
+
+        fetch(`https://odata4.cbs.nl/CBS/${tableid}/Observations`)
+            .then(response => response.json())
+            .then((usefulData) => {
+                setLoading(false);
+                setData(usefulData.value);
+                //setData(usefulData.value.slice(0,9));
+
+            })
+            .catch((e) => {
+                console.error(`An error occurred: ${e}`)
+            });
     };
 
 
-      
-    const handleClickAdd = () => {
-        userDispatch({
-            type: UserActionType.ADD,
-            connection: {
+
+    // const handleClickAdd = () => {
+    //     userDispatch({
+    //         type: UserActionType.ADD,
+    //         connection: {
+    //             nodeID: nanoid(),
+    //             name: 'CBS Table Observation',
+    //             description: 'beschrijving',
+    //             type: 'odata4',
+    //             connectorConfiguration: {
+    //                 url: ''
+    //             }
+    //         },
+    //     });
+    // }
+
+    const AddNode = () => {
+        const x:FlowNode = {
+                nodeID: nanoid(),
+                nodeClass: nodeClass.SOURCENODE,
                 name: 'CBS Table Observation',
                 description: 'beschrijving',
                 type: 'odata4',
-                connectorConfiguration: {
-                    url: ''
-                }
-            },
-          });
+                // properties: {
+                //     url: ''
+                // }
+            }
+        
+        flowActionDispatch({
+            type: FlowActionType.ADDNODE,
+            node: x
+        });
     }
 
-  
+    const deleteNode = (nodeid: string) => {
+        flowActionDispatch({
+            type: FlowActionType.DELETENODE,
+            nodeID: nodeid
+            }
+        );
+    }
+
+    // const handleClickDelete = (nodeid: string) => {
+    //     userDispatch({
+    //         type: UserActionType.DELETE,
+    //         nodeID: nodeid
+    //         }
+    //     );
+    // }
+
+
     function checkResponse(data: any) {
-      if (data) {
-        for (let i = 0; i < data.length; i++) {
-          const obj = data[i];
-  
-          //  console.log(obj);
+        if (data) {
+            for (let i = 0; i < data.length; i++) {
+                const obj = data[i];
+
+                //  console.log(obj);
+            }
+
+            return <p>Fetched</p>;
+        } else {
+            return null;
         }
-  
-        return <p>Fetched</p>;
-      } else {
-        return null;
-      }
     }
     const classes = useStyles();
 
@@ -130,11 +255,11 @@ const DOEN: React.FC = () => {
         <div id='doen' className={classes.dit}>
             <h1>Import Connections</h1>
             <ul>
-                {imports.map(dataTable => (
-                    <li key={dataTable.name}>{dataTable.name} <Button size="small">delete</Button></li>
+                {flowDocument.nodes.filter(node => node.nodeClass === nodeClass.SOURCENODE).map(node => (
+                    <li key={node.name}>{node.name} <Button size="small" onClick={() => deleteNode(node.nodeID)}>delete</Button></li>
                 ))}
             </ul>
-            <Button onClick={handleClickAdd}>Addx</Button>
+            <Button onClick={AddNode}>Addx</Button>
             <Dialog>
                 <DialogTrigger disableButtonEnhancement>
                     <Button>Add</Button>
@@ -143,7 +268,7 @@ const DOEN: React.FC = () => {
                     <DialogBody>
                         <DialogTitle>Dialog title</DialogTitle>
                         <DialogContent>
-                            <ConnectorPicker/>
+                            <ConnectorPicker />
                         </DialogContent>
                         <DialogActions>
                             <DialogTrigger disableButtonEnhancement>
@@ -159,10 +284,9 @@ const DOEN: React.FC = () => {
 
             <h1>Export Connections</h1>
             <ul>
-                {/* {dataTables.map(dataTable => (
-                    <li key={dataTable.name}>{dataTable.name} <Button size="small">delete</Button></li>
-                ))} */}
-                <li key={"spss"}>SPSS OUTPUT.SAV <Button size="small">delete</Button></li>
+                {flowDocument.nodes.filter(node => node.nodeClass === nodeClass.EXPORTNODE).map(node => (
+                    <li key={node.name}>{node.name} <Button size="small" onClick={() => deleteNode(node.nodeID)}>delete</Button></li>
+                ))}
             </ul>
             <Dialog>
                 <DialogTrigger disableButtonEnhancement>
@@ -172,7 +296,7 @@ const DOEN: React.FC = () => {
                     <DialogBody>
                         <DialogTitle>Dialog title</DialogTitle>
                         <DialogContent>
-                            <ConnectorPicker/>
+                            <ConnectorPicker />
                         </DialogContent>
                         <DialogActions>
                             <DialogTrigger disableButtonEnhancement>
@@ -186,16 +310,16 @@ const DOEN: React.FC = () => {
             {/* <ConnectorPicker/> */}
             <br></br>
             <h1>Flow options</h1>
-            <Button onClick={handleClick}>RUN FLOW</Button>  
-            <Button>SCHEDULE</Button> 
-        {/* <button type="submit" onClick={handleClick}>Connect</button> */}
-        {/* {checkResponse(data)} */}
-        <div className="App">
-          {loading && <p>Loading...</p>}
-          <p>
-          {checkResponse(data) && <div > <h4>Observations</h4><Tabulate arr={data} /> </div>}
-        </p>
-        </div>          
+            <Button onClick={handleClick}>RUN FLOW</Button>
+            <Button>SCHEDULE</Button>
+            {/* <button type="submit" onClick={handleClick}>Connect</button> */}
+            {/* {checkResponse(data)} */}
+            <div className="App">
+                {loading && <p>Loading...</p>}
+                <p>
+                    {checkResponse(data) && <div > <h4>Observations</h4><Tabulate arr={data} /> </div>}
+                </p>
+            </div>
         </div>
     );
 }
